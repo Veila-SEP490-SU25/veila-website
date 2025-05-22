624024ddf219d397588ba7b5f52e8216e8aa85fe
neru <ngocngocthuc@gmail.com>
2025-05-23 01:24:55 +0700

    feat: configure Redux store with authApi reducer and middleware

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,9 +1,13 @@
+import { authApi } from "@/services/apis/auth.api";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 setupListeners(store.dispatch);
