541a354c9f446683c237192abb3baead6abfae72
neru <ngocngocthuc@gmail.com>
2025-08-03 14:13:31 +0700

    fix(firebase): remove unused Firebase analytics and FacebookAuthProvider imports

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,11 @@
 import { getFirebaseConfig } from "@/utils";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
-  FacebookAuthProvider,
 } from "firebase/auth";
 
 const getFirebase = () => {
@@ -16,20 +14,15 @@ const getFirebase = () => {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
-  firestore.app.automaticDataCollectionEnabled = true;
   const storage = getStorage(app);
-  const analytics = getAnalytics(app);
   const googleProvider = new GoogleAuthProvider();
-  const facebookProvider = new FacebookAuthProvider();
 
   return {
     app,
     auth,
     firestore,
     storage,
-    analytics,
     googleProvider,
-    facebookProvider,
     signInWithPopup,
   };
 };
