e874b9405e9f0baa34e445b6a2d8607f6fc9fc07
neru <ngocngocthuc@gmail.com>
2025-08-10 16:34:20 +0700

    feat(shop): expand shop API with additional queries and mutations for shop management

--- src/services/apis/shop.api.ts
+++ src/services/apis/shop.api.ts
@@ -1,12 +1,152 @@
 import { baseQueryWithRefresh } from "@/services/apis/base.query";
+import {
+  IAccessory,
+  IBlog,
+  ICategory,
+  ICreateShop,
+  IDress,
+  IGetShopItem,
+  IHandleCreateShop,
+  IItemResponse,
+  IListResponse,
+  IPagination,
+  IService,
+  IShop,
+  IUpdateShopInfo,
+} from "@/services/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const shopApi = createApi({
   reducerPath: "shopApi",
   baseQuery: baseQueryWithRefresh,
   endpoints: (builder) => ({
+    getShops: builder.query<IListResponse<IShop>, IPagination>({
+      query: ({ sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: "shops",
+        method: "GET",
+        params: { sort, filter, page, size },
+      }),
+    }),
 
+    getShop: builder.query<IItemResponse<IShop>, string>({
+      query: (id) => ({
+        url: `shops/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    getShopAccessories: builder.query<IListResponse<IAccessory>, IGetShopItem>({
+      query: ({ id, sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: `shops/${id}/accessories`,
+        method: "GET",
+        params: { page, size, sort, filter },
+      }),
+    }),
+
+    getShopBlogs: builder.query<IListResponse<IBlog>, IGetShopItem>({
+      query: ({ id, sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: `shops/${id}/blogs`,
+        method: "GET",
+        params: { page, size, sort, filter },
+      }),
+    }),
+
+    getShopDresses: builder.query<IListResponse<IDress>, IGetShopItem>({
+      query: ({ id, sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: `shops/${id}/dresses`,
+        method: "GET",
+        params: { page, size, sort, filter },
+      }),
+    }),
+
+    getShopCategories: builder.query<IListResponse<ICategory>, IGetShopItem>({
+      query: ({ id, sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: `shops/${id}/categories`,
+        method: "GET",
+        params: { page, size, sort, filter },
+      }),
+    }),
+
+    getShopServices: builder.query<IListResponse<IService>, IGetShopItem>({
+      query: ({ id, sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: `shops/${id}/services`,
+        method: "GET",
+        params: { page, size, sort, filter },
+      }),
+    }),
+
+    staffGetShops: builder.query<IListResponse<IShop>, IPagination>({
+      query: ({ sort = "", filter = "", page = 1, size = 10 }) => ({
+        url: "shops/staff",
+        method: "GET",
+        params: { sort, filter, page, size },
+      }),
+    }),
+
+    staffGetShop: builder.query<IItemResponse<IShop>, string>({
+      query: (id) => ({
+        url: `shops/${id}/staff`,
+        method: "GET",
+      }),
+    }),
+
+    getMyShop: builder.query<IItemResponse<IShop>, void>({
+      query: () => ({
+        url: "shops/me",
+        method: "GET",
+      }),
+    }),
+
+    staffHandleCreateShop: builder.mutation<
+      IItemResponse<IShop>,
+      IHandleCreateShop
+    >({
+      query: ({ id, ...body }) => ({
+        url: `shops/${id}staff`,
+        method: "POST",
+        body,
+      }),
+    }),
+
+    createShop: builder.mutation<IItemResponse<IShop>, ICreateShop>({
+      query: (body) => ({
+        url: "shops/me",
+        method: "POST",
+        body,
+      }),
+    }),
+
+    recreateShop: builder.mutation<IItemResponse<IShop>, ICreateShop>({
+      query: (body) => ({
+        url: "shops/me",
+        method: "PUT",
+        body,
+      }),
+    }),
+
+    updateShopInfo: builder.mutation<IItemResponse<IShop>, IUpdateShopInfo>({
+      query: (body) => ({
+        url: "shops/me",
+        method: "PATCH",
+        body,
+      }),
+    }),
   }),
 });
 
-export const {} = shopApi;
+export const {
+  useLazyGetMyShopQuery,
+  useLazyGetShopAccessoriesQuery,
+  useLazyGetShopCategoriesQuery,
+  useLazyGetShopBlogsQuery,
+  useLazyGetShopDressesQuery,
+  useLazyGetShopServicesQuery,
+  useLazyGetShopQuery,
+  useLazyGetShopsQuery,
+  useLazyStaffGetShopQuery,
+  useLazyStaffGetShopsQuery,
+  useCreateShopMutation,
+  useRecreateShopMutation,
+  useUpdateShopInfoMutation,
+  useStaffHandleCreateShopMutation,
+} = shopApi;
--- src/services/types/shop.type.ts
+++ src/services/types/shop.type.ts
@@ -1,15 +1,14 @@
-import { IItem } from "@/services/types";
+import { IItem, IPagination } from "@/services/types";
 
 export enum ShopStatus {
-  PENDING = 'PENDING', // Đang chờ duyệt
-  ACTIVE = 'ACTIVE', // Đang hoạt động
-  INACTIVE = 'INACTIVE', // Tạm ngưng hoạt động (do chủ shop)
-  SUSPENDED = 'SUSPENDED', // Tạm ngưng (do admin)
-  BANNED = 'BANNED', // Bị cấm hoạt động
+  PENDING = "PENDING", // Đang chờ duyệt
+  ACTIVE = "ACTIVE", // Đang hoạt động
+  INACTIVE = "INACTIVE", // Tạm ngưng hoạt động (do chủ shop)
+  SUSPENDED = "SUSPENDED", // Tạm ngưng (do admin)
+  BANNED = "BANNED", // Bị cấm hoạt động
 }
 
-export interface IShop extends IItem
-{
+export interface IShop extends IItem {
   name: string;
   phone: string;
   email: string;
@@ -20,4 +19,33 @@ export interface IShop extends IItem
   status: ShopStatus;
   reputation: number;
   isVerified: boolean;
+}
+
+export interface IGetShopItem extends IPagination {
+  id: string;
+}
+
+export interface IHandleCreateShop {
+  id: string;
+  isApproved: boolean;
+  rejectReason: string | null;
+}
+
+export interface ICreateShop {
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+  licenseImages: string;
+}
+
+export interface IUpdateShopInfo {
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+  description: string;
+  logoUrl: string;
+  coverUrl: string;
+  images: string;
 }
