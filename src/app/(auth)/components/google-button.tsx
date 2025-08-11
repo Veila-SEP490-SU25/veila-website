d1782192740ce51756608985f42795d70e749b4c
neru <ngocngocthuc@gmail.com>
2025-08-11 21:20:19 +0700

    ref: remove unused import

--- src/app/(auth)/components/google-button.tsx
+++ src/app/(auth)/components/google-button.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { Image } from "@/components/image";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/providers/auth.provider";
-import { useGoogleAuthMutation } from "@/services/apis";
 import { useFirebase } from "@/services/firebase";
 import { useCallback } from "react";
 import { toast } from "sonner";
