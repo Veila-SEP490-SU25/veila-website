0f172956b9abf0539295d84151bb67f5bbac00c6
neru <ngocngocthuc@gmail.com>
2025-08-14 15:09:56 +0700

    feat(chat): add ChatPage component for chat functionality

---
+++ src/hooks/use-firestore.ts
@@ -0,0 +1,49 @@
+"use client";
+import { useFirebase } from "@/services/firebase";
+import {
+  collection,
+  FieldPath,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+  WhereFilterOp,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+export interface Condition {
+  field: string | FieldPath;
+  operator: WhereFilterOp;
+  value: string | number;
+}
+
+export const useFirstore = (collectionPath: string, condition?: Condition) => {
+  const [document, setDocument] = useState<any[]>([]);
+  const { firestore } = useFirebase();
+  
+  useEffect(() => {
+    if (!firestore) return;
+    let collectionRef = query(
+      collection(firestore, collectionPath),
+      orderBy("createdAt", "desc")
+    );
+    if (condition) {
+      collectionRef = query(
+        collectionRef,
+        where(condition.field, condition.operator, condition.value)
+      );
+    }
+
+    const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
+      const data: any[] = [];
+      snapshot.docs.forEach((doc) => {
+        data.push({ ...doc.data(), docId: doc.id });
+      });
+      setDocument(data);
+    });
+
+    return () => unsubscribe();
+  }, [collectionPath, condition]);
+
+  return document;
+};
