2f322d2930708564549f0fc332de9b6e8bac0e07
neru <ngocngocthuc@gmail.com>
2025-08-29 00:16:11 +0700

    Refactor code structure for improved readability and maintainability

--- src/services/apis/membership.api.ts
+++ src/services/apis/membership.api.ts
@@ -1,12 +1,36 @@
 import { baseQueryWithRefresh } from "@/services/apis/base.query";
+import { IItemResponse, IMembership } from "@/services/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
+
+export interface IRegisterMembership {
+  subscriptionId: string;
+  force: boolean;
+  otp: string;
+}
 
 export const membershipApi = createApi({
   reducerPath: "membershipApi",
   baseQuery: baseQueryWithRefresh,
   endpoints: (builder) => ({
+    registerMembership: builder.mutation<
+      IItemResponse<IMembership>,
+      IRegisterMembership
+    >({
+      query: (body) => ({
+        url: "/membership/register",
+        method: "POST",
+        body,
+      }),
+    }),
 
-  })
-})
+    cancelMembership: builder.mutation<IItemResponse<IMembership>, void>({
+      query: () => ({
+        url: "/membership/cancel",
+        method: "PUT",
+      }),
+    }),
+  }),
+});
 
-export const {} = membershipApi;
+export const { useRegisterMembershipMutation, useCancelMembershipMutation } =
+  membershipApi;
--- src/services/apis/order.api.ts
+++ src/services/apis/order.api.ts
@@ -1,17 +1,11 @@
 import { baseQueryWithRefresh } from "@/services/apis/base.query";
 import {
-  IComplaint,
-  ICreateComplaint,
   ICreateCustomOrder,
   ICreateOrder,
-  IGetComplaints,
   IItemResponse,
   IListResponse,
   IOrder,
-  IOrderDressDetail,
   IPagination,
-  IUpdateOrderInfo,
-  IUpdateOrderStatus,
 } from "@/services/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
@@ -19,24 +13,12 @@ export const orderApi = createApi({
   reducerPath: "orderApi",
   baseQuery: baseQueryWithRefresh,
   endpoints: (builder) => ({
+    // Get Orders
     getOrders: builder.query<IListResponse<IOrder>, IPagination>({
-      query: ({ sort = "", filter = "", page = 0, size = 10 }) => ({
-        url: `orders`,
-        method: "GET",
-        params: {
-          sort,
-          filter,
-          page,
-          size,
-        },
-      }),
-    }),
-
-    createOrder: builder.mutation<IItemResponse<IOrder>, ICreateOrder>({
-      query: (body) => ({
+      query: ({ filter = "", sort = "", page = 0, size = 10 }) => ({
         url: "orders",
-        method: "POST",
-        body,
+        method: "GET",
+        params: { filter, sort, page, size },
       }),
     }),
 
@@ -47,114 +29,28 @@ export const orderApi = createApi({
       }),
     }),
 
-    updateOrderInfo: builder.mutation<IItemResponse<IOrder>, IUpdateOrderInfo>({
-      query: ({ id, ...body }) => ({
-        url: `orders/${id}`,
-        method: "PUT",
-        body,
-      }),
-    }),
-
-    getMyOrderComplaints: builder.query<
-      IListResponse<IComplaint>,
-      IGetComplaints
-    >({
-      query: ({ id, filter = "", page = 0, size = 10, sort = "" }) => ({
-        url: `orders/${id}/complaints/me`,
-        method: "GET",
-        params: {
-          filter,
-          page,
-          size,
-          sort,
-        },
-      }),
-    }),
-
-    createOrderComplaint: builder.mutation<
-      IItemResponse<IComplaint>,
-      ICreateComplaint
-    >({
-      query: ({ id, ...body }) => ({
-        url: `orders/${id}/complaints/me`,
+    //Create Order
+    createOrder: builder.mutation<IItemResponse<IOrder>, ICreateOrder>({
+      query: (body) => ({
+        url: "orders",
         method: "POST",
         body,
-      }),
-    }),
-
-    updateOrderStatus: builder.mutation<
-      IItemResponse<IOrder>,
-      IUpdateOrderStatus
-    >({
-      query: ({ id, status }) => ({
-        url: `orders/${id}/${status}`,
-        method: "PUT",
-      }),
-    }),
-
-    getCustomerOrders: builder.query<IListResponse<IOrder>, IPagination>({
-      query: ({ sort = "", filter = "", page = 0, size = 10 }) => ({
-        url: `orders/customer`,
-        method: "GET",
-        params: {
-          sort,
-          filter,
-          page,
-          size,
-        },
-      }),
-    }),
-
-    getShopOrders: builder.query<IListResponse<IOrder>, IPagination>({
-      query: ({ sort = "", filter = "", page = 0, size = 10 }) => ({
-        url: `orders/shop`,
-        method: "GET",
-        params: {
-          sort,
-          filter,
-          page,
-          size,
-        },
-      }),
-    }),
-
-    getOrderDressDetail: builder.query<
-      IListResponse<IOrderDressDetail>,
-      string
-    >({
-      query: (id) => ({
-        url: `orders/${id}/order-dress-details`,
-        method: "GET",
-      }),
-    }),
-
-    checkoutOrder: builder.mutation<IItemResponse<IOrder>, string>({
-      query: (id) => ({
-        url: `orders/${id}/check-out`,
-        method: "PUT",
       }),
     }),
 
     createCustomOrder: builder.mutation<IItemResponse<IOrder>, ICreateCustomOrder>({
       query: (body) => ({
-        url: `orders/custom`,
+        url: "orders/custom",
         method: "POST",
         body,
-      }),
+      }), 
     })
   }),
 });
 
 export const {
   useLazyGetOrdersQuery,
-  useCreateOrderMutation,
   useLazyGetOrderQuery,
-  useUpdateOrderStatusMutation,
-  useLazyGetCustomerOrdersQuery,
-  useLazyGetMyOrderComplaintsQuery,
-  useLazyGetShopOrdersQuery,
-  useCreateOrderComplaintMutation,
-  useLazyGetOrderDressDetailQuery,
-  useCheckoutOrderMutation,
-  useCreateCustomOrderMutation
+  useCreateOrderMutation,
+  useCreateCustomOrderMutation,
 } = orderApi;
