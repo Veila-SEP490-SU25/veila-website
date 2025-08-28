b1b74a52080f6250f3617d91f171493962f6c5a3
neru <ngocngocthuc@gmail.com>
2025-08-29 05:46:45 +0700

    feat(user): add user creation, update, deletion, and retrieval APIs

--- src/services/apis/user.api.ts
+++ src/services/apis/user.api.ts
@@ -1,6 +1,30 @@
 import { baseQueryWithRefresh } from "@/services/apis/base.query";
-import { IIdentifyUser, IItemResponse } from "@/services/types";
+import {
+  IIdentifyUser,
+  IItemResponse,
+  IListResponse,
+  IPagination,
+  IUser,
+  UserRole,
+  UserStatus,
+} from "@/services/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
+
+export interface ICreateUser {
+  username: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  status: UserStatus;
+  isVerified: boolean;
+  isIdentified: boolean;
+}
+
+export interface IUpdateUser extends ICreateUser {
+  id: string;
+}
 
 export const userApi = createApi({
   reducerPath: "userApi",
@@ -13,7 +37,60 @@ export const userApi = createApi({
         body,
       }),
     }),
+
+    getUsers: builder.query<IListResponse<IUser>, IPagination>({
+      query: ({ sort = "", filter = "", page = 0, size = 10 }) => ({
+        url: "users",
+        method: "GET",
+        params: { sort, filter, page, size },
+      }),
+    }),
+
+    getUser: builder.query<IItemResponse<IUser>, string>({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    createUser: builder.mutation<IItemResponse<IUser>, ICreateUser>({
+      query: (body) => ({
+        url: "users",
+        method: "POST",
+        body,
+      }),
+    }),
+
+    updateUser: builder.mutation<IItemResponse<IUser>, IUpdateUser>({
+      query: (body) => ({
+        url: `users/${body.id}`,
+        method: "PUT",
+        body,
+      }),
+    }),
+
+    deleteUser: builder.mutation<IItemResponse<null>, string>({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "DELETE",
+      }),
+    }),
+
+    restoreUser: builder.mutation<IItemResponse<IUser>, string>({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "PATCH",
+      }),
+    }),
   }),
 });
 
-export const { useIdentifyMutation } = userApi;
+export const {
+  useIdentifyMutation,
+  useLazyGetUsersQuery,
+  useLazyGetUserQuery,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+  useRestoreUserMutation,
+} = userApi;
