1be3ceedde41e967a9a02810024491e8722e6dbc
neru <ngocngocthuc@gmail.com>
2025-08-30 18:53:44 +0700

    fix: rename ChatFirestorePage to ChatPage for consistency

--- src/app/(home)/chat/page.tsx
+++ src/app/(home)/chat/page.tsx
@@ -3,7 +3,7 @@
 import { ChatSidebar } from "@/components/chat/chat-sidebar"
 import { Chatroom } from "@/components/chat/chatroom"
 
-export default function ChatFirestorePage() {
+export default function ChatPage() {
   return (
       <div className="h-screen flex">
         <ChatSidebar />
