2514c6ee1b68eeb42950fa1e93dfe423350a08d1
neru <ngocngocthuc@gmail.com>
2025-09-02 19:46:30 +0700

    feat: implement loading state for StaffSubscriptionPage and enhance its structure

--- src/app/staff/subscriptions/page.tsx
+++ src/app/staff/subscriptions/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import { usePaging } from "@/providers/paging.provider";
+import { useLazyGetSubscriptionsQuery } from "@/services/apis";
+import { ISubscription } from "@/services/types";
+import { useState } from "react";
+
 export default function StaffSubscriptionPage() {
+  const [subscriptions, setSubscriptions] = useState<ISubscription[]>([]);
+  const [trigger, { isLoading }] = useLazyGetSubscriptionsQuery();
+  const { pageIndex, pageSize, totalItems, resetPaging, setPaging } =
+    usePaging();
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<string>("");
+  const [filter, setFilter] = useState<string>("");
+
   return <div>Staff Subscription Page</div>;
 }
